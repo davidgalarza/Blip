76184c164157523a9c99a59535ac7c87a963d672
davidgalarza <vatayo14@gmail.com>
2017-08-15 10:55:33 -0500

    Design featueres added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,7 +15,7 @@ export class MyApp {
   @ViewChild(Nav) nav: Nav;
 
   //rootPage: any;
-  rootPage: any;
+  rootPage: any = HomePage;
   zone:NgZone;
   pages: Array<{title: string, component: any}>;
 
@@ -30,6 +30,7 @@ export class MyApp {
   }
 
   initializeApp() {
+    /*
     // Listen to the auth state and toogling the root page.
     const unsubscribe = this.auth.getAuth().onAuthStateChanged((user) => {
       this.zone.run( () => {
@@ -41,7 +42,7 @@ export class MyApp {
           unsubscribe();
         }
       });     
-      });
+      });*/
     this.platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
